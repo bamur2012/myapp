ec878cdcf67662a89d529ea4ca20030cc9fb5804
bamur2012 <bamur2012@gmail.com>
2024-02-28 16:47:06 +0500

    201209

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -49,16 +49,16 @@
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
- #server "192.168.31.37",
-#   user: "bamur",
-#   roles: %w{web db app}
+ server "192.168.31.37",
+   user: "bamur",
+   roles: %w{web db app}
 
 
-   set :stage, :production
+#   set :stage, :production
 
-role :app, %w[bamur@192.168.31.37]
-role :web, %w[bamur@192.168.31.37]
-role :db,  %w[bamur@192.168.31.37]
+#role :app, %w[bamur@192.168.31.37]
+#role :web, %w[bamur@192.168.31.37]
+#role :db,  %w[bamur@192.168.31.37]
 
 set :rvm_ruby_version, "3.0.1@#{fetch(:application)}"
 set :rails_env, :production
