46579128e95067de9865c9aedb8c3ca7c23bbfea
bamur2012 <bamur2012@gmail.com>
2024-02-28 22:10:52 +0500

    23423

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,23 +1,16 @@
 # config valid only for current version of Capistrano
-lock '3.18.0'
+lock '3.15.0'
 
-set :user, 'bamur'
-stt :application, 'myapp'
-set :rvm_ruby_version, '3.0.1'
-
+set :application, 'myapp'
 set :repo_url, 'git@github.com:bamur2012/myapp.git'
-set :deploy_to, "/home/#{fetch(:user)}/#{fetch(:application)}"
-#set :deploy_to, "/home/bamur/myapp/current"
+set :deploy_to, "/home/bamur/#{fetch(:application)}"
+set :user, 'bamur'
 
-
-
-#append :linked_files 'config/database.yml', 'config/secrets.yml'
-set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w[log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads]
 set :whenever_identifier, -> { "#{fetch(:application)}_#{fetch(:stage)}" }
 
 set :puma_threads, [4, 16]
 set :puma_workers, 2
-
 
 set :pty,             false
 set :use_sudo,        false
@@ -27,16 +20,16 @@ set :deploy_via,      :remote_cache
 # Don't change these unless you know what you're doing
 set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w[~/.ssh/id_rsa.pub] }
 
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,       "#{shared_path}/tmp/pids/puma.pid"
+set :puma_bind,       'unix:///home/bamur/myapp/shared/tmp/sockets/myapp-puma.sock'
+set :puma_state,      '/home/bamur/myapp/shared/tmp/pids/puma.state'
+set :puma_pid,        '/home/bamur/myapp/shared/tmp/pids/puma.pid'
 
-set :puma_access_log,  "#{release_path}/log/puma.error.log"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true # Change to false when not using ActiveRecord
-set :branch, "main"
+
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
   task :make_dirs do
@@ -48,6 +41,7 @@ namespace :puma do
 
   before :start, :make_dirs
 end
+
 namespace :deploy do
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
@@ -57,11 +51,12 @@ namespace :deploy do
       # end
     end
   end
- desc 'Make sure local git is in sync with remote.'
+
+  desc 'Make sure local git is in sync with remote.'
   task :check_revision do
     on roles(:app) do
       unless `git rev-parse HEAD` == `git rev-parse origin/main`
-        puts 'WARNING: HEAD is not the same as origin/main'
+        puts 'WARNING: HEAD is not the same as origin/master'
         puts 'Run `git push` to sync changes.'
         exit
       end
